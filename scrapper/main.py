00fff8b1886d164cc3f585de01ea727528011403
PatrykOstasz <patryk.ostasz@gmail.com>
2023-03-16 02:23:23 +0100

    [Scrapper] Scrapping all product names with prices
    beautifulsoup scrapps data and shows them in simple table
    with pandas. Additional .ipynb file was created for jupyter.

--- scrapper/main.py
+++ scrapper/main.py
@@ -3,12 +3,28 @@ import time
 from bs4 import BeautifulSoup
 from contextlib import closing
 from selenium.webdriver import Edge
+import pandas as pd
 
-url = "https://allegro.pl"
+def convertFromWsToWebWs(name):
+    return name.replace(" ", "%20")
 
+search = "listing?string="
+buynow = "offerTypeBuyNow=1"
+logicaland = "&"
+searchName = "Final Fantasy"
+
+url = "https://allegro.pl/" + search + convertFromWsToWebWs(searchName) + logicaland + buynow
 
 with closing(Edge()) as browser:
     browser.get(url)
     time.sleep(5)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
-    print(soup.prettify())
+
+itemsWithPrices = []
+for item in soup.find_all('article'):
+    tempList = str(item.get('aria-label')).split(", ")
+    if tempList != ['None']:
+        itemsWithPrices.append(tempList)
+
+data = pd.DataFrame(itemsWithPrices, columns=['name', 'price', 'x', 'y', 'z'])
+print(data)
