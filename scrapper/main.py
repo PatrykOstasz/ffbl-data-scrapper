7ceb6dd0507c6e998f111f0892461a7d07b5d6b6
PatrykOstasz <patryk.ostasz@gmail.com>
2023-04-07 13:55:52 +0200

    Rewriting code to not use BS4
    Selenium also has similar functionality to BS4
    therefore BS4 can be removed.
    
    Additionally added some hacks for WAF.

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,28 +1,52 @@
 import time
-
-from bs4 import BeautifulSoup
-from contextlib import closing
-from selenium.webdriver import Edge
 import pandas as pd
+import random
 
-def convertFromWsToWebWs(name):
-    return name.replace(" ", "%20")
+from selenium.webdriver import Edge
+from selenium.webdriver import EdgeOptions
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+#this is for future testing
+#from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 search = "listing?string="
 buynow = "offerTypeBuyNow=1"
 logicaland = "&"
 searchName = "Final Fantasy"
 
-url = "https://allegro.pl/" + search + convertFromWsToWebWs(searchName) + logicaland + buynow
+url = "https://allegro.pl/" + search + searchName + logicaland + buynow
 
-with closing(Edge()) as browser:
-    browser.get(url)
-    time.sleep(5)
-    soup = BeautifulSoup(browser.page_source, 'html.parser')
+edge_options = EdgeOptions()
+edge_options.use_chromium = True
+
+#for now those have to be commented-out
+#edge_options.add_argument('headless')
+#edge_options.add_argument('disable-gpu')
+
+# Adding argument to disable the AutomationControlled flag 
+edge_options.add_argument('disable-blink-features=AutomationControlled')
+
+# Exclude the collection of enable-automation switches 
+edge_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+
+# Turn-off userAutomationExtension 
+edge_options.add_experimental_option("useAutomationExtension", False) 
+
+driver = Edge(service=Service('config/msedgedriver.exe'), options=edge_options)
+
+# Changing the property of the navigator value for webdriver to undefined
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+time.sleep(random.randint(1, 5))
+
+driver.get(url)
+
+wait = WebDriverWait(driver, random.randint(1, 5)) 
 
 itemsWithPrices = []
-for item in soup.find_all('article'):
-    tempList = str(item.get('aria-label')).split(", ")
+for element in driver.find_elements(By.TAG_NAME, 'article'):
+    tempList = str(element.get_attribute('aria-label')).split(", ")
     if tempList != ['None']:
         itemsWithPrices.append(tempList)
 
